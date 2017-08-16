9a818d49031be89dda38d874c279c47c0b34cf46
Roberto Higor <higor15762@live.com>
2017-08-16 16:39:32 -0300

    mini conexão

--- src/Connection.php
+++ src/Connection.php
@@ -1,13 +1,30 @@
 <?php
-Class Connection{
-    function connectar(){
-            $mysqli = mysqli_connect("localhost", "root", "26793653", "academico");
-             if (mysqli_connect_errno($mysqli)) {
-                  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-            }
+//Class Connection{   
+    //public function __construct(){
+    $mysqli = new mysqli("localhost", "root", "26793653", "academico");
+    
+    if ($mysqli->connect_errno) {
+                  echo "Failed to connect to MySQL: " . $mysqli->connect_error();
     }
-}
-$c = new Connection;
-$c->connectar();
+  
+    if (!$mysqli->query("DROP TABLE IF EXISTS test") ||
+        !$mysqli->query("CREATE TABLE test(id INT)") ||
+        !$mysqli->query("INSERT INTO test(id) VALUES (1)")) {
+            echo "Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+    //}
+//}
+
+//Class Teste{
+    
+   //echo $mysqli->host_info . "\n";
+
+    $res = $mysqli->query("SELECT * FROM ALUNO ORDER BY nome ASC");
+
+    // ponteiro para 0 $res->data_seek(0);
+    while ($row = $res->fetch_assoc()) {
+        echo "Nome : " . $row['nome'] . "</br>";
+    }
+//}
 ?>
 
